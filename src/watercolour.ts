1e09b2e3b86f243e6f20d7b2d8cbe6c9e25687e5
Alex Griffiths <alex-griffiths@outlook.com>
2018-12-24 16:24:48 +1100

    Updated function to add noise to the polygon and layer a series of the polygons on top each other.

--- src/watercolour.ts
+++ src/watercolour.ts
@@ -13,80 +13,80 @@ class WaterColour{
     // Draws a polygon at a random x/y position with a random amount of sides with a random size.
     drawPolygon(context: CanvasRenderingContext2D){
         // Define the constant variables that are used to draw the polygon.
-        const min_sides: number =  5;
-        const max_sides: number = 20;
+        const min_sides: number =  6;
+        const max_sides: number = 8;
         const min_size: number = 10;
         const max_size: number = 200;
-        const polygon_x_center: number = Math.floor(Math.random() * (1900 - 25 + 1));
-        const polygon_y_center: number = Math.floor(Math.random() * (1020 - 25 + 1));
-        const r: number = Math.floor(Math.random() * Math.floor(255));
-        const g: number = Math.floor(Math.random() * Math.floor(255));
-        const b: number = Math.floor(Math.random() * Math.floor(255));
+        // const polygon_x_center: number = Math.floor(Math.random() * (1900 - 25 + 1));
+        // const polygon_y_center: number = Math.floor(Math.random() * (1020 - 25 + 1));
         const opacity: number = Math.random() * Math.floor(1);
         const num_sides = Math.floor(Math.random() * (max_sides - min_sides + 1)) + min_sides;
-        const size: number = Math.floor(Math.random() * (max_size - min_size + 1)) + min_size;
-        const xCenter: number = polygon_x_center + size;
-        const yCenter: number = polygon_y_center + size;
-
+        // const size: number = Math.floor(Math.random() * (max_size - min_size + 1)) + min_size;
+        const size: number = 200;
+        const xCenter: number = 100 + size;
+        const yCenter: number = 100 + size;
+        
         // Here we set the fill style, the blend mode, and begin drawing the polygon based on a series of points.   
         // This code is based on this post - http://scienceprimer.com/drawing-regular-polygons-javascript-canvas
         // The only tweak I really made to this was to modify to allow for the above variables to be passed in
         // rather than using hard coded values.
         const points: number[][] = new Array();
-        context.fillStyle = `rgb(${r}, ${g}, ${b}, ${opacity})`;
-        context.globalCompositeOperation = "lighten";
+        // context.globalCompositeOperation = "lighten";
         
         for(let i = 0; i <= num_sides; i++){
             let x: number = xCenter + (size * Math.cos(i * 2 * Math.PI / num_sides));
             let y: number = yCenter + (size * Math.sin(i * 2 * Math.PI / num_sides));
             points.push([x, y]);
         }
+        const r: number = Math.floor(Math.random() * Math.floor(255));
+        const g: number = Math.floor(Math.random() * Math.floor(255));
+        const b: number = Math.floor(Math.random() * Math.floor(255));
         
-        for(let i = 0; i < 5; i++){
-            let points_with_noise: number[][] = this.addNoiseToPolygon(points, 0);
+        for(let i = 0; i < 100; i++){
+            let deformed_base_polygon: number[][] = this.addNoiseToPolygon(points, 0, 5);
+            context.fillStyle = `rgb(${r}, ${g}, ${b}, 0.04)`;
             context.beginPath();
-            context.moveTo(points_with_noise[0][0], points_with_noise[0][1]);
-            points_with_noise.forEach(xy => {
+            context.moveTo(deformed_base_polygon[0][0], deformed_base_polygon[0][1]);
+            deformed_base_polygon.forEach(xy => {
                 context.lineTo(xy[0], xy[1]);
             })
             context.closePath();
             context.fill();
         }
     }
-
+    
     // Take an array of co-ordinates, and add 
-    addNoiseToPolygon(points: number[][], recursion_depth: number){
-        const recursion_limit = 6;
+    addNoiseToPolygon(points: number[][], recursion_depth: number, recursion_limit: number){
         let points_with_noise: number[][] = new Array();
-        points.forEach((xy, index) => {
-            if(index + 1 < points.length){
-                let midpoint_x: number = (xy[0] + points[index+1][0])/2;
-                let midpoint_y: number = (xy[1] + points[index+1][1])/2;
-                let x1_diff_squared = Math.pow(xy[0] - midpoint_x, 2);
-                let x2_diff_squared = Math.pow(points[index + 1][0] - midpoint_x, 2);
-                let y1_diff_squared = Math.pow(xy[1] - midpoint_y, 2);
-                let y2_diff_squared = Math.pow(points[index + 1][1] - midpoint_y, 2);
-                let x_sum_of_diffs_sqaured = x1_diff_squared + x2_diff_squared;
-                let y_sum_of_diffs_squared = y1_diff_squared + y2_diff_squared;
-                let x_variance = x_sum_of_diffs_sqaured/3;
-                let y_variance = y_sum_of_diffs_squared/3;
-                let sigma_x = Math.sqrt(x_variance);
-                let sigma_y = Math.sqrt(y_variance);
-
-                let x3: number = Math.random() * ((midpoint_x + sigma_x) - (midpoint_x - sigma_x) + 1) + (midpoint_x - sigma_x);
-                let y3: number = Math.random() * ((midpoint_y + sigma_y) - (midpoint_y - sigma_y) + 1) + (midpoint_y - sigma_y);
-
-                points_with_noise.push([xy[0], xy[1]]);
-                points_with_noise.push([x3, y3]);
-            }
-        })
-
-        // if(recursion_depth < recursion_limit){
-        //     recursion_depth++;
-        //     this.addNoiseToPolygon(points_with_noise, recursion_depth);
-        // } else {
-            return points_with_noise;
-        // }
+        if(recursion_depth < recursion_limit){
+            points.forEach((xy, index) => {
+                if(index + 1 < points.length){
+                    let midpoint_x: number = (xy[0] + points[index+1][0])/2;
+                    let midpoint_y: number = (xy[1] + points[index+1][1])/2;
+                    let x1_diff_squared = Math.pow(xy[0] - midpoint_x, 2);
+                    let x2_diff_squared = Math.pow(points[index + 1][0] - midpoint_x, 2);
+                    let y1_diff_squared = Math.pow(xy[1] - midpoint_y, 2);
+                    let y2_diff_squared = Math.pow(points[index + 1][1] - midpoint_y, 2);
+                    let x_sum_of_diffs_sqaured = x1_diff_squared + x2_diff_squared;
+                    let y_sum_of_diffs_squared = y1_diff_squared + y2_diff_squared;
+                    let x_variance = x_sum_of_diffs_sqaured/3;
+                    let y_variance = y_sum_of_diffs_squared/3;
+                    let sigma_x = Math.sqrt(x_variance);
+                    let sigma_y = Math.sqrt(y_variance);
+    
+                    let x3: number = Math.random() * ((midpoint_x + sigma_x) - (midpoint_x - sigma_x) + 1) + (midpoint_x - sigma_x);
+                    let y3: number = Math.random() * ((midpoint_y + sigma_y) - (midpoint_y - sigma_y) + 1) + (midpoint_y - sigma_y);
+    
+                    points_with_noise.push([xy[0], xy[1]]);
+                    points_with_noise.push([x3, y3]);
+                }
+            })
+            recursion_depth++;
+            points_with_noise = this.addNoiseToPolygon(points_with_noise, recursion_depth, recursion_limit);
+        } else {
+            points_with_noise = points;
+        }
+        return points_with_noise;
     }
 }
 
@@ -106,8 +106,8 @@ document.addEventListener("DOMContentLoaded", function(event){
     context.scale(pixel_ratio, pixel_ratio);
 
     let waterColour = new WaterColour(canvas, context);
+    waterColour.drawPolygon(context);
     
-    setInterval(() => {
-        waterColour.drawPolygon(context)
-    }, 2500);
+    // setInterval(() => {
+    // }, 2500);
 })
--- watercolour.js
+++ watercolour.js
@@ -14,37 +14,38 @@ var WaterColour = /** @class */ (function () {
     // Draws a polygon at a random x/y position with a random amount of sides with a random size.
     WaterColour.prototype.drawPolygon = function (context) {
         // Define the constant variables that are used to draw the polygon.
-        var min_sides = 5;
-        var max_sides = 20;
+        var min_sides = 6;
+        var max_sides = 8;
         var min_size = 10;
         var max_size = 200;
-        var polygon_x_center = Math.floor(Math.random() * (1900 - 25 + 1));
-        var polygon_y_center = Math.floor(Math.random() * (1020 - 25 + 1));
-        var r = Math.floor(Math.random() * Math.floor(255));
-        var g = Math.floor(Math.random() * Math.floor(255));
-        var b = Math.floor(Math.random() * Math.floor(255));
+        // const polygon_x_center: number = Math.floor(Math.random() * (1900 - 25 + 1));
+        // const polygon_y_center: number = Math.floor(Math.random() * (1020 - 25 + 1));
         var opacity = Math.random() * Math.floor(1);
         var num_sides = Math.floor(Math.random() * (max_sides - min_sides + 1)) + min_sides;
-        var size = Math.floor(Math.random() * (max_size - min_size + 1)) + min_size;
-        var xCenter = polygon_x_center + size;
-        var yCenter = polygon_y_center + size;
+        // const size: number = Math.floor(Math.random() * (max_size - min_size + 1)) + min_size;
+        var size = 200;
+        var xCenter = 100 + size;
+        var yCenter = 100 + size;
         // Here we set the fill style, the blend mode, and begin drawing the polygon based on a series of points.   
         // This code is based on this post - http://scienceprimer.com/drawing-regular-polygons-javascript-canvas
         // The only tweak I really made to this was to modify to allow for the above variables to be passed in
         // rather than using hard coded values.
         var points = new Array();
-        context.fillStyle = "rgb(" + r + ", " + g + ", " + b + ", " + opacity + ")";
-        context.globalCompositeOperation = "lighten";
+        // context.globalCompositeOperation = "lighten";
         for (var i = 0; i <= num_sides; i++) {
             var x = xCenter + (size * Math.cos(i * 2 * Math.PI / num_sides));
             var y = yCenter + (size * Math.sin(i * 2 * Math.PI / num_sides));
             points.push([x, y]);
         }
-        for (var i = 0; i < 5; i++) {
-            var points_with_noise = this.addNoiseToPolygon(points, 0);
+        var r = Math.floor(Math.random() * Math.floor(255));
+        var g = Math.floor(Math.random() * Math.floor(255));
+        var b = Math.floor(Math.random() * Math.floor(255));
+        for (var i = 0; i < 100; i++) {
+            var deformed_base_polygon = this.addNoiseToPolygon(points, 0, 5);
+            context.fillStyle = "rgb(" + r + ", " + g + ", " + b + ", 0.04)";
             context.beginPath();
-            context.moveTo(points_with_noise[0][0], points_with_noise[0][1]);
-            points_with_noise.forEach(function (xy) {
+            context.moveTo(deformed_base_polygon[0][0], deformed_base_polygon[0][1]);
+            deformed_base_polygon.forEach(function (xy) {
                 context.lineTo(xy[0], xy[1]);
             });
             context.closePath();
@@ -52,35 +53,36 @@ var WaterColour = /** @class */ (function () {
         }
     };
     // Take an array of co-ordinates, and add 
-    WaterColour.prototype.addNoiseToPolygon = function (points, recursion_depth) {
-        var recursion_limit = 6;
+    WaterColour.prototype.addNoiseToPolygon = function (points, recursion_depth, recursion_limit) {
         var points_with_noise = new Array();
-        points.forEach(function (xy, index) {
-            if (index + 1 < points.length) {
-                var midpoint_x = (xy[0] + points[index + 1][0]) / 2;
-                var midpoint_y = (xy[1] + points[index + 1][1]) / 2;
-                var x1_diff_squared = Math.pow(xy[0] - midpoint_x, 2);
-                var x2_diff_squared = Math.pow(points[index + 1][0] - midpoint_x, 2);
-                var y1_diff_squared = Math.pow(xy[1] - midpoint_y, 2);
-                var y2_diff_squared = Math.pow(points[index + 1][1] - midpoint_y, 2);
-                var x_sum_of_diffs_sqaured = x1_diff_squared + x2_diff_squared;
-                var y_sum_of_diffs_squared = y1_diff_squared + y2_diff_squared;
-                var x_variance = x_sum_of_diffs_sqaured / 3;
-                var y_variance = y_sum_of_diffs_squared / 3;
-                var sigma_x = Math.sqrt(x_variance);
-                var sigma_y = Math.sqrt(y_variance);
-                var x3 = Math.random() * ((midpoint_x + sigma_x) - (midpoint_x - sigma_x) + 1) + (midpoint_x - sigma_x);
-                var y3 = Math.random() * ((midpoint_y + sigma_y) - (midpoint_y - sigma_y) + 1) + (midpoint_y - sigma_y);
-                points_with_noise.push([xy[0], xy[1]]);
-                points_with_noise.push([x3, y3]);
-            }
-        });
-        // if(recursion_depth < recursion_limit){
-        //     recursion_depth++;
-        //     this.addNoiseToPolygon(points_with_noise, recursion_depth);
-        // } else {
+        if (recursion_depth < recursion_limit) {
+            points.forEach(function (xy, index) {
+                if (index + 1 < points.length) {
+                    var midpoint_x = (xy[0] + points[index + 1][0]) / 2;
+                    var midpoint_y = (xy[1] + points[index + 1][1]) / 2;
+                    var x1_diff_squared = Math.pow(xy[0] - midpoint_x, 2);
+                    var x2_diff_squared = Math.pow(points[index + 1][0] - midpoint_x, 2);
+                    var y1_diff_squared = Math.pow(xy[1] - midpoint_y, 2);
+                    var y2_diff_squared = Math.pow(points[index + 1][1] - midpoint_y, 2);
+                    var x_sum_of_diffs_sqaured = x1_diff_squared + x2_diff_squared;
+                    var y_sum_of_diffs_squared = y1_diff_squared + y2_diff_squared;
+                    var x_variance = x_sum_of_diffs_sqaured / 3;
+                    var y_variance = y_sum_of_diffs_squared / 3;
+                    var sigma_x = Math.sqrt(x_variance);
+                    var sigma_y = Math.sqrt(y_variance);
+                    var x3 = Math.random() * ((midpoint_x + sigma_x) - (midpoint_x - sigma_x) + 1) + (midpoint_x - sigma_x);
+                    var y3 = Math.random() * ((midpoint_y + sigma_y) - (midpoint_y - sigma_y) + 1) + (midpoint_y - sigma_y);
+                    points_with_noise.push([xy[0], xy[1]]);
+                    points_with_noise.push([x3, y3]);
+                }
+            });
+            recursion_depth++;
+            points_with_noise = this.addNoiseToPolygon(points_with_noise, recursion_depth, recursion_limit);
+        }
+        else {
+            points_with_noise = points;
+        }
         return points_with_noise;
-        // }
     };
     return WaterColour;
 }());
@@ -97,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     context.imageSmoothingEnabled = false; /// future
     context.scale(pixel_ratio, pixel_ratio);
     var waterColour = new WaterColour(canvas, context);
-    setInterval(function () {
-        waterColour.drawPolygon(context);
-    }, 2500);
+    waterColour.drawPolygon(context);
+    // setInterval(() => {
+    // }, 2500);
 });
